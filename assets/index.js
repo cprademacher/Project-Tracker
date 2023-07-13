693a0f090070ec1f8250a857b3f49c6178e8e6ed
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-07-13 09:57:04 -0500

    Added a header with content

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,7 @@
-console.log('hello');
+var updateTime = () => {
+        var today = dayjs().format("MMM D, YYYY @ hh:mm:ss a");
+        var showDateEl = $('#current-time');
+        showDateEl.text(today);
+};
+updateTime();
+setInterval(updateTime, 1000);
